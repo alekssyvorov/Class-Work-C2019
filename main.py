ae63caa3577011b691085b50f7ead111b6818e6e
Djekson <alekssyvorov29@gmail.com>
2024-01-20 14:46:46 +0200

    Version program 5.0

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-# This is a sample Python script.
+class Student:
+    group = "C2019"
+    gender = "Male"
+    education = "MKA STEP"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name, age, swim):
+        # print(id(self))
+        self.name = name
+        self.age = age
+        self.swim = swim
+
+    def get_swim(self):
+        return self.swim
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+st1 = Student(name="Oleg", age=15, swim=True)
+st2 = Student(name='Anna', age=17, swim=True)
+st3 = Student(name="Illya", age=16, swim=False)
+print(st1.name, st1.get_swim())
+print(st2.name, st2.get_swim())
+print(st3.name, st3.get_swim())
+
+# print(st1.name)
+# print(st1.age)
+# print(st1.education)
+# st2 = Student()
+# st2.name = "Anna"
+# print(st2.name)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# print(id(st1))
+# st2 = Student()
 
+# print(st1.group, st1.gender, st1.education)
+# print(st2.group, st2.gender, st2.education)
+# print()
+# st2.group = "C2020"
+#
+# print(st1.group, st1.gender, st1.education)
+# print(st2.group, st2.gender, st2.education)
